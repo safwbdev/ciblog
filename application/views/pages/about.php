720b9cdd862bd3d04f8448127a3a1dd244457429
Stef <safwbdev@gmail.com>
2017-09-25 11:20:52 +0800

    first commit

---
+++ application/views/pages/about.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>This is the first thing I have done</p>
---
+++ application/views/posts/index.php
@@ -0,0 +1,26 @@
+<!--
+For the Posts
+-->
+
+<h2><?= $title ?></h2>
+
+<?php foreach($posts as $post) : ?>
+  <h3><?php echo $post['title']; ?></h3>
+  <div class="row">
+    <div class="col-md-3">
+      <img src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image']; ?>" class="post-thumb">
+    </div>
+    <div class="col-md-9">
+      <small class="post-date">Posted on: <?php echo $post['created_at']; ?> in <strong><?php echo $post['name']; ?></strong></small><br />
+        <?php echo word_limiter($post['body'], 30); ?>
+        <br />
+        <br />
+        <p><a class="btn btn-default" href="<?php echo site_url('posts/'.$post['slug']); ?>">Read More</a></p>
+    </div>
+  </div>
+
+  <?php endforeach; ?>
+
+  <div class="pagination-links">
+    <?php echo $this->pagination->create_links(); ?>
+  </div>
